7754235d3726b24c44e91ebdb13fbd79d6794080
Marco Bügling <marco.buegling@mailbox.org>
2026-04-16 23:52:59 +0200

    Added start-stop functionality using the stby pin

--- src/robot/control/four_wheel_car_control.py
+++ src/robot/control/four_wheel_car_control.py
@@ -1,3 +1,8 @@
+import sys
+if sys.platform == "linux":
+    import RPi.GPIO as GPIO
+else:
+    GPIO = None
 from robot.hardware.motors import MotorGroup, Motor
 from collections import overload
 
@@ -10,6 +15,7 @@ class FourWheelCarControl:
     def __init__(
         self, 
         base_speed : float, 
+        stby: int,
         pwma_l: int, 
         ain1_l: int, 
         ain2_l: int, 
@@ -22,11 +28,11 @@ class FourWheelCarControl:
         pwmb_r: int, 
         bin1_r: int, 
         bin2_r: int,
-
     ):
         """
         Args:
             base_speed: values 0 and 100, defines max duty cycles for motors
+            stby: stby pin shared by all motor drivers
             pwma_l: pwm pin of front left motor
             ain1_l: in1 pin of front left motor
             ain2_l: in2 pin of front left motor
@@ -48,6 +54,9 @@ class FourWheelCarControl:
 
         self._motors_left = MotorGroup("LEFT", [Motor("FL", pwma_l, ain1_l, ain2_l), Motor("RL", pwmb_l, bin1_l, bin2_l)], self._base_speed)
         self._motors_right = MotorGroup("RIGHT", [Motor("FR", pwma_r, ain1_r, ain2_r), Motor("RR", pwmb_r, bin1_r, bin2_r)], self._base_speed)
+        self._stby = stby
+
+        self.start()
 
     def _update_motor_speed(self):
         rel_speed_left = self._clamp(self._speed - self._steering, -1.0, 1.0)
@@ -82,6 +91,18 @@ class FourWheelCarControl:
         """
         self._steering = self._clamp(self._steering + change, -1.0, 1.0)
         self._update_motor_speed()
+
+    def start(self):
+        """
+        Activate motor drivers by setting a high signal on the stby pin
+        """
+        GPIO.output(self._stby, True)
+
+    def stop(self):
+        """
+        Set car into stby mode, stopping completely until start() is called
+        """
+        GPIO.output(self._stby, False)
 
     @staticmethod
     def _clamp(value: float, lo: float, hi: float) -> float:
